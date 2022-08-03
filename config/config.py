3291dfb785ce34e4f1c768f52de92110fd1f7440
luzhangao <luzhangao@gmail.com>
2022-08-02 20:36:17 -0400

    Add a email tool

---
+++ config/config.py
@@ -0,0 +1,21 @@
+# coding:utf8
+
+"""
+@author: Zhangao Lu
+@contact: zlu2@laurentian.ca
+@time: 2022/8/2
+@description:
+"""
+
+from decouple import config
+
+
+SENDER_MAIL_HOST = config('SENDER_MAIL_HOST')
+SENDER_MAIL_USER = config('SENDER_MAIL_USER')
+SENDER_MAIL_PASS = config('SENDER_MAIL_PASS')
+
+RECEIVER_MAIL_USER = config('RECEIVER_MAIL_USER')
+
+
+if __name__ == '__main__':
+    pass
